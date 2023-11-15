3d659b7cb8ac6c9f39067da3209ccf9c9e920aab
soheegoo <soheegoo@gmail.com>
2023-11-14 21:49:25 -0500

    added chat messages

--- applications/admin.py
+++ applications/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models.applications import Application
+from .models.chat import Chat
 
+admin.site.register(Chat)
 admin.site.register(Application)
--- shelters/urls.py
+++ shelters/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from rest_framework_simplejwt.views import TokenObtainPairView
-from .views.shelter import PetShelterSignUpView, PetShelterLoginView,ShelterRetrieveUpdateDestroyView
+from .views.shelter import PetShelterSignUpView,ShelterRetrieveUpdateDestroyView
 from .views.shelter import PetShelterListView
 from .views.pets import CreatePetView, PetDetailView
 from .views.reviews import CreateListView, CreateReviewMessageView, MessageListAPIView
@@ -10,7 +10,7 @@ app_name = 'shelter'
 
 urlpatterns = [
     path('account/', PetShelterSignUpView.as_view(), name='signup'),
-    path('token/', PetShelterLoginView.as_view(), name='login'),
+    path('token/', TokenObtainPairView.as_view(), name='login'),
     path('<int:shelter_pk>/', ShelterRetrieveUpdateDestroyView.as_view(), name='detail'),
     path('', PetShelterListView.as_view(), name='list'),
     path('<int:shelter_pk>/review/', CreateListView.as_view()),
