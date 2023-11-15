e4ce722a383d2f1a20024cfb113aa375281f228d
Julia Bulat <juliabulat77@gmail.com>
2023-11-15 02:21:34 -0500

    apps and pets

--- shelters/views/__init__.py
+++ shelters/views/__init__.py
@@ -1,3 +1,2 @@
 from .shelter import PetShelterSignUpView,ShelterRetrieveUpdateDestroyView,PetShelterListView,ShelterImageCreateView,ShelterImageDeleteView
 from .reviews import CreateReviewMessageView,CreateListView,MessageListAPIView
-from .pets import PetImageCreateView,PetImageDeleteView
